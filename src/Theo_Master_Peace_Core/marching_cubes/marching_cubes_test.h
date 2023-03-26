75087959b36fc8511d80799082ca272c946744e4
TheoLomeuBraga <theolomeu@gmail.com>
2023-03-26 17:49:41 -0300

    a

--- src/Theo_Master_Peace_Core/marching_cubes/marching_cubes_test.h
+++ src/Theo_Master_Peace_Core/marching_cubes/marching_cubes_test.h
@@ -6,3 +6,15 @@ std::map<unsigned int, marching_cubes::Mesh> marching_cubes_test(){
     mct.setDensity(1,1,1,1);
     return marchingCubesToMeshWithTypes(mct);
 }
+std::map<unsigned int, marching_cubes::Mesh> marching_cubes_test2(){
+
+    marching_cubes::MarchingCubesTerrain mct(6,6,6,1);
+
+    mct.setDensity(1,1,1,1);
+    mct.setType(1,1,1,1);
+
+    mct.setDensity(2,1,1,2);
+    mct.setType(2,1,1,2);
+    
+    return marchingCubesToMeshWithTypes(mct);
+}
