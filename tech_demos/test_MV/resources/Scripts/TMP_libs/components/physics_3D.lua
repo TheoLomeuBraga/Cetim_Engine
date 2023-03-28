0e0d85e6d067be04e6f6c7d5b323b445b2c9b77c
TheoLomeuBraga <theolomeu@gmail.com>
2023-03-28 16:01:07 -0300

    a

--- tech_demos/test_MV/resources/Scripts/TMP_libs/components/physics_3D.lua
+++ tech_demos/test_MV/resources/Scripts/TMP_libs/components/physics_3D.lua
@@ -1,9 +1,8 @@
 require("TMP_libs.components.component_index")
 
 
-function get_set_physic_3D(get_set,object) 
+function get_set_physic_3D(get_set, object)
 end
-
 
 --boady_dynamics
 boady_dynamics = {}
@@ -21,7 +20,7 @@ colision_layer_info = {}
 function colision_layer_info:new()
     cli = {}
     cli.layer = 1
-    cli.layers_can_colide = {1,}
+    cli.layers_can_colide = { 1, }
     return cli
 end
 
@@ -29,16 +28,36 @@ physics_3D_component = {}
 function physics_3D_component:new(object_ptr)
     p = {}
     p.object_ptr = object_ptr
+    p.scale = { x = 1, y = 1, z = 1 }
+    p.boady_dynamic = boady_dynamics.static
+    p.colision_shape = colision_shapes.tile
+    p.rotate = true
+    p.triger = false
+    p.friction = 1
+    p.objects_coliding = {}
+    p.colision_layer = colision_layer_info:new()
+    p.colision_mesh = { file = "", name = "" };
     function p:get()
-        j = get_set_physic_3D(get_lua,self.object_ptr)
-        
+        j = get_set_physic_3D(get_lua, self.object_ptr)
+        self.scale = deepcopyjson(j.scale)
+        self.boady_dynamic = j.boady_dynamic
+        self.colision_shape = j.colision_shape
+        self.rotate = j.rotate
+        self.triger = j.triger
+        self.friction = j.friction
+        self.objects_coliding = deepcopyjson(j.objects_coliding)
+        self.colision_layer = deepcopyjson(j.colision_layer)
+        self.colision_mesh = deepcopyjson(j.colision_mesh)
     end
-    function p:set()
 
-        get_set_physic_3D(set_lua,deepcopyjson(self))
+    function p:set()
+        get_set_physic_3D(set_lua, deepcopyjson(self))
     end
+
     function p:delet()
     end
+
     return p
 end
+
 component_map[components.physics_3D] = physics_3D_component
