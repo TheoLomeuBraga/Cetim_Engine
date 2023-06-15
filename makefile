2f708b326011d1be72bca011bea6c6097c3797b5
TheoLomeuBraga <theolomeu@gmail.com>
2023-06-15 17:03:44 -0300

    a

--- makefile
+++ makefile
@@ -27,33 +27,37 @@ LIBS_FONT_READER_WINDOWS := -lfreetype `pkg-config --cflags --libs gtk+-3.0`
 LIBS_FONT_READER_LINUX := -lfreetype `pkg-config --cflags --libs gtk+-3.0`
 
 windows: 
-	rm -f $(TARGET_ENGINE_WINDOWS) $(TARGET_FONT_READER_WINDOWS) 
+	rm -f $(TARGET_ENGINE_WINDOWS) $(TARGET_FONT_READER_WINDOWS) ./build/font_reader_gtk.glade ./build/theme.css
 	cp ./src/font_reader_gtk/font_reader_gtk.glade ./build/font_reader_gtk.glade
+	cp ./src/font_reader_gtk/theme.css ./build/theme.css
 
 	$(COMPILER) $(SRC_ENGINE) $(INCLUDE_DIRS) $(FLAGS_WINDOWS) $(DEFINITIONS_WINDOWS) $(LIBS_ENGINE_WINDOWS) -o $(TARGET_ENGINE_WINDOWS)
 	$(COMPILER) $(SRC_READER) $(INCLUDE_DIRS) $(FLAGS_WINDOWS) $(DEFINITIONS_WINDOWS) $(LIBS_FONT_READER_LINUX) -o $(TARGET_FONT_READER_WINDOWS)
 
 windows_debug: 
-	rm -f $(TARGET_ENGINE_WINDOWS) $(TARGET_FONT_READER_WINDOWS) 
+	rm -f $(TARGET_ENGINE_WINDOWS) $(TARGET_FONT_READER_WINDOWS)  ./build/font_reader_gtk.glade ./build/theme.css
 	cp ./src/font_reader_gtk/font_reader_gtk.glade ./build/font_reader_gtk.glade
+	cp ./src/font_reader_gtk/theme.css ./build/theme.css
 
 	$(COMPILER) $(SRC_ENGINE) $(INCLUDE_DIRS) $(FLAGS_WINDOWS_DEBUG) $(DEFINITIONS_WINDOWS) $(LIBS_ENGINE_WINDOWS) -o $(TARGET_ENGINE_WINDOWS)
 	$(COMPILER) $(SRC_READER) $(INCLUDE_DIRS) $(FLAGS_WINDOWS_DEBUG) $(DEFINITIONS_WINDOWS) $(LIBS_FONT_READER_LINUX) -o $(TARGET_FONT_READER_WINDOWS)
 
 arch:
+	rm -f $(TARGET_ENGINE_LINUX) $(TARGET_FONT_READER_LINUX) ./build/font_reader_gtk.glade ./build/theme.css
 	cp ./src/font_reader_gtk/font_reader_gtk.glade ./build/font_reader_gtk.glade
-	rm -f $(TARGET_ENGINE_LINUX) $(TARGET_FONT_READER_LINUX)
+	cp ./src/font_reader_gtk/theme.css ./build/theme.css
 
 	$(COMPILER) $(SRC_ENGINE) $(INCLUDE_DIRS) $(FLAGS_LINUX) $(DEFINITIONS_LINUX)  $(LIBS_ENGINE_ARCH) -o $(TARGET_ENGINE_LINUX)
 	$(COMPILER) $(SRC_READER) $(INCLUDE_DIRS) $(FLAGS_LINUX) $(DEFINITIONS_LINUX)  $(LIBS_FONT_READER_LINUX) -o $(TARGET_FONT_READER_LINUX)
 
 debian:
+	rm -f $(TARGET_ENGINE_LINUX) $(TARGET_FONT_READER_LINUX) ./build/font_reader_gtk.glade ./build/theme.css
 	cp ./src/font_reader_gtk/font_reader_gtk.glade ./build/font_reader_gtk.glade
-	rm -f $(TARGET_ENGINE_LINUX) $(TARGET_FONT_READER_LINUX)
+	cp ./src/font_reader_gtk/theme.css ./build/theme.css
 
 	$(COMPILER) $(SRC_ENGINE) $(INCLUDE_DIRS) $(FLAGS_LINUX) $(DEFINITIONS_LINUX)  $(LIBS_ENGINE_DEBIAN) -o $(TARGET_ENGINE_LINUX)
 	$(COMPILER) $(SRC_READER) $(INCLUDE_DIRS) $(FLAGS_LINUX) $(DEFINITIONS_LINUX)  $(LIBS_FONT_READER_LINUX) -o $(TARGET_FONT_READER_LINUX)
 
 clean:
-	rm -f $(TARGET_ENGINE_WINDOWS) $(TARGET_ENGINE_LINUX) $(TARGET_FONT_READER_WINDOWS) $(TARGET_FONT_READER_LINUX) ./build/font_reader_gtk.glade
+	rm -f $(TARGET_ENGINE_WINDOWS) $(TARGET_ENGINE_LINUX) $(TARGET_FONT_READER_WINDOWS) $(TARGET_FONT_READER_LINUX) ./build/font_reader_gtk.glade ./build/theme.css
 
--- src/font_reader_gtk/font_reader_gtk.cpp
+++ src/font_reader_gtk/font_reader_gtk.cpp
@@ -4,23 +4,24 @@
 
 #include <fstream>
 
-bool file_exists(const std::string& filename) {
+bool file_exists(const std::string &filename)
+{
     std::ifstream file(filename.c_str());
     return file.good();
 }
 
-GtkBuilder* builder,*builder_popup;
+GtkBuilder *builder, *builder_popup;
 
-
-
-void close_window(GtkButton *button, gpointer user_data) {
-    gtk_widget_destroy((GtkWidget*)user_data);
+void close_window(GtkButton *button, gpointer user_data)
+{
+    gtk_widget_destroy((GtkWidget *)user_data);
 }
 
-void error_popup(std::string error){
-    
+void error_popup(std::string error)
+{
+
     builder_popup = gtk_builder_new_from_file("font_reader_gtk.glade");
-    //GtkWidget *button_show_popup = GTK_WIDGET(gtk_builder_get_object(builder_popup, "button_show_popup"));
+    // GtkWidget *button_show_popup = GTK_WIDGET(gtk_builder_get_object(builder_popup, "button_show_popup"));
 
     // Obtem um ponteiro para a janela do popup
     GtkWidget *popup = GTK_WIDGET(gtk_builder_get_object(builder_popup, "error_popup"));
@@ -36,90 +37,106 @@ void error_popup(std::string error){
     gtk_widget_show_all(popup);
 
     g_object_unref(builder_popup);
-    
 }
 
-std::string fontFilePath = ""; 
+std::string fontFilePath = "";
 std::string jsonFilePath = "";
 std::string jsonFileName = "font.json";
 bool pixel_perfect = true;
 int quality = 40;
 
-void apply(GtkButton *apply_button, gpointer user_data){
-    bool fontFilePathExist = file_exists(fontFilePath.c_str()),qualityAboveZero = quality > 0;
+void apply(GtkButton *apply_button, gpointer user_data)
+{
+    bool fontFilePathExist = file_exists(fontFilePath.c_str()), qualityAboveZero = quality > 0;
 
     std::cout << jsonFilePath + std::string("/") + jsonFileName << std::endl;
-    
-    if(fontFilePathExist && qualityAboveZero){
-        storeCharsInfoInJSON(fontFilePath.c_str(),  (jsonFilePath + std::string("/") + jsonFileName).c_str(), pixel_perfect, quality);
-    }else{
-        if(fontFilePathExist == false){
+
+    if (fontFilePathExist && qualityAboveZero)
+    {
+        storeCharsInfoInJSON(fontFilePath.c_str(), (jsonFilePath + std::string("/") + jsonFileName).c_str(), pixel_perfect, quality);
+    }
+    else
+    {
+        if (fontFilePathExist == false)
+        {
             error_popup("\nerror path not exist\n");
         }
-        if(qualityAboveZero == false){
+        if (qualityAboveZero == false)
+        {
             error_popup("\nerror quality should be biger than zero\n");
         }
     }
 }
 
-
-
-void set_fontFilePath(GtkFileChooser* text_input, gpointer user_data){
-    fontFilePath = gtk_file_chooser_get_preview_filename (text_input);
+void set_fontFilePath(GtkFileChooser *text_input, gpointer user_data)
+{
+    fontFilePath = gtk_file_chooser_get_preview_filename(text_input);
     std::cout << fontFilePath << std::endl;
 }
 
-void set_jsonFilePath(GtkFileChooser* text_input, gpointer user_data){
-    jsonFilePath = gtk_file_chooser_get_filename (text_input);
+void set_jsonFilePath(GtkFileChooser *text_input, gpointer user_data)
+{
+    jsonFilePath = gtk_file_chooser_get_filename(text_input);
     std::cout << jsonFilePath << std::endl;
 }
 
-void set_jsonFileName(GtkEntry* text_input, gpointer user_data){
+void set_jsonFileName(GtkEntry *text_input, gpointer user_data)
+{
     jsonFileName = gtk_entry_get_text(text_input);
     std::cout << jsonFileName << std::endl;
 }
 
-void set_quality(GtkEntry* text_input, gpointer user_data){
+void set_quality(GtkEntry *text_input, gpointer user_data)
+{
     quality = atof(gtk_entry_get_text(text_input));
     std::cout << quality << std::endl;
 }
 
-void set_pixel_perfect(GtkWidget* widget, gpointer data){
+void set_pixel_perfect(GtkWidget *widget, gpointer data)
+{
     pixel_perfect = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget));
     std::cout << pixel_perfect << std::endl;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     gtk_init(&argc, &argv);
 
     // Criando o builder a partir do arquivo Glade
     builder = gtk_builder_new_from_file("font_reader_gtk.glade");
 
-    // Obtendo um objeto GtkWidget do builder
-    GtkWidget* window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    if (file_exists("theme.css"))
+    {
+        GtkCssProvider *provider = gtk_css_provider_new();
+        GdkDisplay *display = gdk_display_get_default();
+        GdkScreen *screen = gdk_display_get_default_screen(display);
+        gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+        gtk_css_provider_load_from_path(provider, "theme.css", NULL);
+    }
 
-    GtkWidget* font_file_input_fild = GTK_WIDGET(gtk_builder_get_object(builder, "font_file"));
+    // Obtendo um objeto GtkWidget do builder
+    GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+
+    GtkWidget *font_file_input_fild = GTK_WIDGET(gtk_builder_get_object(builder, "font_file"));
     g_signal_connect(font_file_input_fild, "file-set", G_CALLBACK(set_fontFilePath), NULL);
 
-
-    GtkWidget* output_folder_input_fild = GTK_WIDGET(gtk_builder_get_object(builder, "output_folder"));
+    GtkWidget *output_folder_input_fild = GTK_WIDGET(gtk_builder_get_object(builder, "output_folder"));
     g_signal_connect(output_folder_input_fild, "file-set", G_CALLBACK(set_jsonFilePath), NULL);
 
-    GtkWidget* file_name_input_fild = GTK_WIDGET(gtk_builder_get_object(builder, "file_name"));
+    GtkWidget *file_name_input_fild = GTK_WIDGET(gtk_builder_get_object(builder, "file_name"));
     g_signal_connect(file_name_input_fild, "changed", G_CALLBACK(set_jsonFileName), NULL);
 
-    GtkWidget* set_quality_fild = GTK_WIDGET(gtk_builder_get_object(builder, "quality"));
+    GtkWidget *set_quality_fild = GTK_WIDGET(gtk_builder_get_object(builder, "quality"));
     g_signal_connect(set_quality_fild, "changed", G_CALLBACK(set_quality), NULL);
 
-    GtkWidget* pixel_perfect_fild = GTK_WIDGET(gtk_builder_get_object(builder, "pixel_perfect"));
+    GtkWidget *pixel_perfect_fild = GTK_WIDGET(gtk_builder_get_object(builder, "pixel_perfect"));
     g_signal_connect(pixel_perfect_fild, "state-set", G_CALLBACK(set_pixel_perfect), NULL);
 
-    GtkWidget* apply_button = GTK_WIDGET(gtk_builder_get_object(builder, "apply"));
+    GtkWidget *apply_button = GTK_WIDGET(gtk_builder_get_object(builder, "apply"));
     g_signal_connect(apply_button, "clicked", G_CALLBACK(apply), NULL);
 
     // Mostrando a janela
     gtk_widget_show_all(window);
-    
 
     gtk_main();
 
@@ -128,4 +145,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
