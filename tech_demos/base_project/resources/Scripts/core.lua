75622f43ce2e4b8a9a69adfa6d0e77ba62e770a7
TheoLomeuBraga <theolomeu@gmail.com>
2023-07-10 12:57:34 -0300

    a

--- tech_demos/base_project/resources/Scripts/core.lua
+++ tech_demos/base_project/resources/Scripts/core.lua
@@ -34,27 +34,6 @@ function set_resolution()
     window:set()
 end
 
-current_scene = {}
-function load_map(map)
-    current_scene = nil
-    current_scene = require("game_maps.".. map ..".map" )
-    current_scene:load()
-    global_data:set_var("current_scene_name",map)
-end
-
-
-
-function unload_map()
-    current_scene:unload()
-end
-
-function load_map_from_other(map)
-    unload_map()
-    current_scene = nil
-    load_map(map[1])
-    return {}
-end
-
 
 
 function START()
@@ -66,17 +45,10 @@ function START()
 
     test_get_scene_3D = get_scene_3D("resources/3D Models/cube.gltf")
 
-    load_map("test_level_first_person")
-
-    --load_map("test_level")
-
     
 end
 
 function UPDATE()
-    if current_scene.update ~= nil then
-        current_scene:update()
-    end
 
     if keys_axis:get_input(input_devices.keyboard, input_keys.keyboard[input_keys.keyboard.delete]) == 1  then
         window:close()
