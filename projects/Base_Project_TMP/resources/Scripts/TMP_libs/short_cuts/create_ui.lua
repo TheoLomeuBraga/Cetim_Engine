24ae011de4fcb73598e83114c5d1c18c40e11ccf
TheoLomeuBraga <theolomeu@gmail.com>
2023-08-08 14:28:24 -0300

    a

--- projects/Base_Project_TMP/resources/Scripts/TMP_libs/short_cuts/create_ui.lua
+++ projects/Base_Project_TMP/resources/Scripts/TMP_libs/short_cuts/create_ui.lua
@@ -4,50 +4,69 @@ require("TMP_libs.components.transform")
 require("TMP_libs.components.render_shader")
 require("TMP_libs.short_cuts.create_render_shader")
 
+style = {}
+function style:new()
+    return {
 
+        color = { r = 1, g = 0, b = 0, a = 1 },
+        color_hover = { r = 0, g = 0, b = 1, a = 1 },
+        color_click = { r = 0, g = 0, b = 1, a = 1 },
+        background_image = "resources/Textures/white.png",
 
-function create_button(father,is_ui,pos,rot,sca,layer,mat)
+        text_font = "resources/Fonts/Glowworm Regular.json",
+        text_color = { r = 0, g = 1, b = 0, a = 1 },
+        text_color_hover = { r = 0, g = 0, b = 1, a = 1 },
+        text_color_click = { r = 0, g = 0, b = 1, a = 1 },
 
-    ret = game_object:new(create_object(father))
+        border_size = 0.5,
+        border_roundnes = 0.5,
+        border_color = { r = 0, g = 0, b = 1, a = 1 },
+        border_color_hover = { r = 0, g = 0, b = 1, a = 1 },
+        border_color_click = { r = 0, g = 0, b = 1, a = 1 },
 
-    ret:add_component(components.transform)
-    ret.components[components.transform].is_ui = is_ui
-    ret.components[components.transform].position = deepcopy(pos)
-    ret.components[components.transform].rotation = deepcopy(rot)
-    ret.components[components.transform].scale = deepcopy(sca)
-    ret.components[components.transform]:set()
-
-    ret:add_component(components.render_shader)
-    ret.components[components.render_shader].layer = layer
-    ret.components[components.render_shader].material = deepcopy(mat)
-    ret.components[components.render_shader]:set()
-
-    return ret
-
+    }
 end
 
-function create_text_ui(father,is_ui,pos,rot,sca,layer,mat)
+local ui_object_example = {
 
-    ret = game_object:new(create_object(father))
+    obj = {},
 
-    ret:add_component(components.transform)
-    ret.components[components.transform].is_ui = is_ui
-    ret.components[components.transform].position = deepcopy(pos)
-    ret.components[components.transform].rotation = deepcopy(rot)
-    ret.components[components.transform].scale = deepcopy(sca)
-    ret.components[components.transform]:set()
+    text = "hello world",
 
-    ret:add_component(components.render_shader)
-    ret.components[components.render_shader].layer = layer
-    ret.components[components.render_shader].material = deepcopy(mat)
-    ret.components[components.render_shader]:set()
+    click_function = nil,
+
+    style = style:new()
+
+}
+
+function create_ui(father,is_ui,pos,sca,text,click_function,style)
+
+    --object
+    ret = {
+        obj = game_object:new(create_object(father)),
+        text = text,
+        click_function = click_function,
+        style = style
+    }
+
+    --transform
+    ret.obj:add_component(components.transform)
+    function ret:set_transform(is_ui,pos,sca)
+        ret.obj.components[components.transform].is_ui = is_ui
+        ret.obj.components[components.transform].position = deepcopy(pos)
+        ret.obj.components[components.transform].scale = deepcopy(sca)
+        ret.obj.components[components.transform]:set()
+    end
+    ret:set_transform(is_ui,pos,sca)
+    
+    --button
+
+    
 
     return ret
-    
 end
 
-function create_input_fild(father,is_ui,pos,rot,sca,layer,mat)
-
+function create_button(father, is_ui, pos, rot, sca, layer, mat)
     ret = game_object:new(create_object(father))
 
     ret:add_component(components.transform)
@@ -63,5 +82,40 @@ function create_input_fild(father,is_ui,pos,rot,sca,layer,mat)
     ret.components[components.render_shader]:set()
 
     return ret
-    
+end
+
+function create_text_ui(father, is_ui, pos, rot, sca, layer, mat)
+    ret = game_object:new(create_object(father))
+
+    ret:add_component(components.transform)
+    ret.components[components.transform].is_ui = is_ui
+    ret.components[components.transform].position = deepcopy(pos)
+    ret.components[components.transform].rotation = deepcopy(rot)
+    ret.components[components.transform].scale = deepcopy(sca)
+    ret.components[components.transform]:set()
+
+    ret:add_component(components.render_shader)
+    ret.components[components.render_shader].layer = layer
+    ret.components[components.render_shader].material = deepcopy(mat)
+    ret.components[components.render_shader]:set()
+
+    return ret
+end
+
+function create_input_fild(father, is_ui, pos, rot, sca, layer, mat)
+    ret = game_object:new(create_object(father))
+
+    ret:add_component(components.transform)
+    ret.components[components.transform].is_ui = is_ui
+    ret.components[components.transform].position = deepcopy(pos)
+    ret.components[components.transform].rotation = deepcopy(rot)
+    ret.components[components.transform].scale = deepcopy(sca)
+    ret.components[components.transform]:set()
+
+    ret:add_component(components.render_shader)
+    ret.components[components.render_shader].layer = layer
+    ret.components[components.render_shader].material = deepcopy(mat)
+    ret.components[components.render_shader]:set()
+
+    return ret
 end
--- projects/Base_Project_TMP/resources/Scripts/level_loaders/buttons_test.lua
+++ projects/Base_Project_TMP/resources/Scripts/level_loaders/buttons_test.lua
@@ -4,6 +4,8 @@ require("TMP_libs.short_cuts.create_ui")
 
 local demo = {}
 
+demo.menu = {}
+
 demo.text1 = {}
 demo.text2 = {}
 
@@ -13,20 +15,20 @@ demo.button_3 = {}
 
 function demo:START(layers)
 
+    demo.menu = game_object:new(create_object(layers.hud))
+
     local font = "resources/Fonts/Glowworm Regular.json"
     local mat = matreial:new()
-    
-    
 
     mat.shader = "resources/Shaders/text"
     mat.color = {r=0,g=1,b=0,a=1}
-    demo.text1 = create_text(layers.hud,true,{x=-1,y=0.8,z=0},{x=0,y=0,z=0},{x=0.1,y=0.1,z=0.1},mat,4,"just test the \nui buttons",font)
+    demo.text1 = create_text(demo.menu.object_ptr,true,{x=-1,y=0.8,z=0},{x=0,y=0,z=0},{x=0.1,y=0.1,z=0.1},mat,4,"just test the \nui buttons",font)
 
     
     mat.color = {r=1,g=0,b=0,a=1}
     mat.shader = "resources/Shaders/button"
     mat.textures[1] = "resources/Textures/white.png"
-    demo.button_1 = create_button(layers.hud,true,{x=0,y=0,z=0},{x=0,y=0,z=0},{x=1,y=1,z=1},4,mat)
+    demo.button_1 = create_button(demo.menu.object_ptr,true,{x=0,y=0,z=0},{x=0,y=0,z=0},{x=1,y=1,z=1},4,mat)
     
 end
 
@@ -35,8 +37,7 @@ function demo:UPDATE()
 end
 
 function demo:END()
-    remove_object(demo.text1.object_ptr)
-    remove_object(demo.button_1.object_ptr)
+    remove_object(demo.menu.object_ptr)
     clear_memory()
 end
 
