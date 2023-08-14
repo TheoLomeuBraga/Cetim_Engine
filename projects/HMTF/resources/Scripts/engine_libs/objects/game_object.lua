937ee50c0784fb6bea065f9460ab0922491f6170
TheoLomeuBraga <theolomeu@gmail.com>
2023-08-14 18:43:21 -0300

    a

--- projects/HMTF/resources/Scripts/engine_libs/objects/game_object.lua
+++ projects/HMTF/resources/Scripts/engine_libs/objects/game_object.lua
@@ -32,32 +32,37 @@ function get_set_object(get_set,object)
 end
 
 game_object = {}
-function game_object:new(object_ptr)
+function game_object:new(object_ptr,ignore_components)
     obj = {}
     obj.object_ptr = object_ptr
     obj.name = ""
-    obj.components = {}
+    if ignore_components ~= true then
+        obj.components = {}
+    end
     obj.father = ""
     obj.children_ptr = {}
     obj.children = {}
-    function obj:add_component(component_name)
-        add_component(self.object_ptr, component_name)
-        self.components[component_name] = component_map[component_name]:new(self.object_ptr)
-    end
 
-    function obj:remove_component(component_name)
-        remove_component(self.object_ptr, component_name)
-        self.components[component_name]:delet()
-    end
+    if ignore_components ~= true then
+        function obj:add_component(component_name)
+            add_component(self.object_ptr, component_name)
+            self.components[component_name] = component_map[component_name]:new(self.object_ptr)
+        end
 
-    function obj:have_component(comp)
-        return have_component(self.object_ptr, comp) == true
-    end
+        function obj:remove_component(component_name)
+            remove_component(self.object_ptr, component_name)
+            self.components[component_name]:delet()
+        end
 
-    function obj:get_components()
-        for key, value in pairs(components) do
-            if self:have_component(value) then
-                self.components[value] = component_map[value]:new(self.object_ptr)
+        function obj:have_component(comp)
+            return have_component(self.object_ptr, comp) == true
+        end
+
+        function obj:get_components()
+            for key, value in pairs(components) do
+                if self:have_component(value) then
+                    self.components[value] = component_map[value]:new(self.object_ptr)
+                end
             end
         end
     end
@@ -74,21 +79,22 @@ function game_object:new(object_ptr)
         get_set_object(lua_set,self)
     end
     
-
-    function obj:recreate()
-        self:get()
-
-        self.components = {}
-
-        for i, v in pairs(components) do
-            if self:have_component(v) then
-                self.components[v] = component_map[v]:new(self.object_ptr)
-                self.components[v]:get()
+    if ignore_components ~= true then
+        function obj:recreate()
+            self:get()
+    
+            self.components = {}
+    
+            for i, v in pairs(components) do
+                if self:have_component(v) then
+                    self.components[v] = component_map[v]:new(self.object_ptr)
+                    self.components[v]:get()
+                end
             end
         end
+        obj:recreate()
     end
-
-    obj:recreate()
+    
 
     return obj
 end
