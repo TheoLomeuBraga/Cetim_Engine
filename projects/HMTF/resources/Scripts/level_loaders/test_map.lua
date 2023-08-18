2ba6a0223119320889dd98da253dacc1e279475b
TheoLomeuBraga <theolomeu@gmail.com>
2023-08-17 23:17:23 -0300

    a

--- projects/HMTF/resources/Scripts/level_loaders/test_map.lua
+++ projects/HMTF/resources/Scripts/level_loaders/test_map.lua
@@ -19,20 +19,26 @@ local assets_needed = {
 function level:START(layers)
     
     time:set_speed(0)
+    
     core_obj = game_object:new(global_data:get_var("core_object_ptr"))
     core_obj.components[components.lua_scripts]:call_function("core","set_load_image",{path = "resources/Textures/null.png",color={r=1,g=1,b=1}})
 
     local camera = create_camera_perspective(layers.camera, { x = 0, y = 0, z = 0 }, { x = 0, y = 0, z = 0 }, 90, 0.1, 1000)
     set_lisener_object(camera.object_ptr)
-    
-    
+
     stop_coroutine_if_is_not_loaded(assets_needed)
 
     level.scene_3D_data = get_scene_3D("resources/Levels/3D/test_map.gltf")
 
+    coroutine.yield()
+
     cenary_builders.cenary_obj = cenary_builders.scene(layers.hud,level.scene_3D_data,true)
 
+    coroutine.yield()
+
     core_obj.components[components.lua_scripts]:call_function("core","set_load_image",{})
+
+    coroutine.yield()
 
     time:set_speed(1)
 
