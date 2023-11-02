e4f9e726074893fa4373edf82810f8f84dd5289a
TheoLomeuBraga <theolomeu@gmail.com>
2023-11-02 15:54:50 -0300

    a

--- documentation/src/fake_router.js
+++ documentation/src/fake_router.js
@@ -1,2 +1,12 @@
 const main_frame = document.getElementById('main_frame');
 main_frame.src = "./pages/hello.html";
+
+class Button_Linker{
+    constructor(id,page){
+        this.id = id
+        this.page = page
+    }
+}
+buttons_linkers = [
+    new Button_Linker("hello","./pages/hello.html"),
+]
