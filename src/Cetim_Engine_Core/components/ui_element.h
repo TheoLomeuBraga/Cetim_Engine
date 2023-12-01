4c835d7b28d60f68346fb35527c59c74f2cef2ff
TheoLomeuBraga <theolomeu@gmail.com>
2023-12-01 12:42:38 -0300

    a

--- src/Cetim_Engine_Core/components/ui_element.h
+++ src/Cetim_Engine_Core/components/ui_element.h
@@ -4,89 +4,74 @@
 #include <memory>
 using namespace std;
 #include "RecursosT.h"
-#include "game_object.h"
 #include "transform.h"
 #include "ManusearArquivos.h"
 #include "input.h"
 
-enum ui_type
-{
-    none = 0,
-    button = 1,
-    input_text = 2,
-    input_number = 3,
+#include "game_object.h"
+#include "render_shader.h"
+#include "render_text.h"
+
+struct ui_style_struct{
+    vec4 color;
+    vec4 background_color;
+    vec4 border_color;
+
+    float border_size = 1;
+
+    shared_ptr<imagem> background_image;
+    shared_ptr<imagem> border_image;
+
+    shared_ptr<fonte> text_font;
+};
+typedef struct ui_style_struct ui_style;
+
+enum ui_type {
+    lable = 0,
+    text = 1,
+    button = 2,
 };
 
-enum nouse_states
-{
-    out = 0,
-    hover = 1,
-    click = 2,
-};
-
-class ui_element : public componente
+class ui_componente : public componente
 {
 public:
-    // style
-    vec2 position = vec2(0, 0);
-    vec2 size = vec2(0.1, 0.1);
 
-    vec4 color = vec4(0, 0, 0, 1);
-    vec4 back_ground_color = vec4(1, 1, 1, 1);
+    ui_componente *father;
 
-    vec4 color_hover = vec4(0, 0, 0, 1);
-    vec4 back_ground_color_hover = vec4(1, 1, 1, 1);
+    vec2 position;
+    vec2 scale;
 
-    vec4 color_click = vec4(0, 0, 0, 1);
-    vec4 back_ground_color_click = vec4(1, 1, 1, 1);
+    ui_style normal_style;
+    ui_style hover_style;
+    ui_style click_style;
 
-    shared_ptr<imagem> image = NULL;
-    shared_ptr<imagem> background_image = NULL;
+    wstring text;
 
-    unsigned char type = 0;
-    unsigned char mouse_state = 0;
-    string function_lua_id = "";
-    void(function_button)(void);
-
-    string input_text = "";
-    float input_number = 0;
-
-    void(function_input_text)(string);
-    void(function_input_number)(float);
+    shared_ptr<objeto_jogo> text_obj;
+    shared_ptr<objeto_jogo> background_obj;
+    shared_ptr<objeto_jogo> border_obj;
 
     ui_element() {}
 
-    void atualisar()
-    {
+    void iniciar() {
 
-        if (type > 0)
-        {
+        text_obj = novo_objeto_jogo();
+        cena_objetos_selecionados->adicionar_objeto(esse_objeto,text_obj);
 
-            float x = manuseio_inputs->mouse_input.movimentos["normalized_x"];
-            float y = manuseio_inputs->mouse_input.movimentos["normalized_y"];
-            bool click = manuseio_inputs->mouse_input.botoes["left"] > 0;
+        background_obj = novo_objeto_jogo();
+        cena_objetos_selecionados->adicionar_objeto(esse_objeto,background_obj);
 
-            if (mouse_state == click)
-            {
-                switch (type)
-                {
-                case ui_type::button:
-                    function_button();
-                    break;
+        border_obj = novo_objeto_jogo();
+        cena_objetos_selecionados->adicionar_objeto(esse_objeto,border_obj);
 
-                case ui_type::input_text:
-                    function_input_text(input_text);
-                    break;
+    }
 
-                case ui_type::input_number:
-                    function_input_number(input_number);
-                    break;
+	void atualisar() {}
 
-                default:
-                    break;
-                }
-            }
-        }
+	void finalisar() {
+        text_obj->excluir();
+        background_obj->excluir();
+        border_obj->excluir();
     }
 
     ~ui_element() {}
