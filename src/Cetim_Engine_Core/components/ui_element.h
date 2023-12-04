705db1459dff700415b4bfa19375596169af01f5
TheoLomeuBraga <theolomeu@gmail.com>
2023-12-04 10:48:29 -0300

    a

--- src/Cetim_Engine_Core/components/ui_element.h
+++ src/Cetim_Engine_Core/components/ui_element.h
@@ -2,10 +2,9 @@
 
 #include <iostream>
 #include <memory>
+#include <cmath>
 using namespace std;
 #include "RecursosT.h"
-
-
 
 #include "transform.h"
 #include "render_shader.h"
@@ -13,13 +12,13 @@ using namespace std;
 
 #include "LoopPrincipal.h"
 
+struct ui_style_struct
+{
+    vec4 color = vec4(1, 0, 0, 1);
+    vec4 background_color = vec4(0, 1, 0, 1);
+    vec4 border_color = vec4(0, 0, 1, 1);
 
-struct ui_style_struct{
-    vec4 color = vec4(1,0,0,1);
-    vec4 background_color = vec4(0,1,0,1);
-    vec4 border_color = vec4(0,0,1,1);
-
-    float border_size = 0.1;
+    float border_size = 0.05;
 
     shared_ptr<imagem> background_image = NULL;
     shared_ptr<imagem> border_image = NULL;
@@ -28,7 +27,8 @@ struct ui_style_struct{
 };
 typedef struct ui_style_struct ui_style;
 
-enum ui_type {
+enum ui_type
+{
     lable = 0,
     text = 1,
     button = 2,
@@ -37,51 +37,52 @@ enum ui_type {
 class ui_componente : public componente
 {
     shared_ptr<transform_> tf;
-    shared_ptr<objeto_jogo> text_obj,background_obj,border_obj;
-public:
+    shared_ptr<objeto_jogo> text_obj, background_obj, border_obj;
 
+public:
     uint8_t render_layer = 4;
     static vec2 cursor_position;
     uint8_t camada = 0;
     bool ligado = true;
     ui_type type;
-    render_text_location text_location_x,text_location_y;
+    render_text_location text_location_x, text_location_y;
     shared_ptr<ui_componente> father;
-    vec2 global_position = vec2(0,0), position = vec2(0.25,0.25),scale = vec2(0.5,0.5);
-    ui_style normal_style,hover_style,click_style;
+    vec2 global_position = vec2(0, 0), position = vec2(0.5, 0.5), scale = vec2(0.2, 0.2);
+    ui_style normal_style, hover_style, click_style;
     wstring text;
 
     ui_componente() {}
 
-    void iniciar() {
+    void iniciar()
+    {
 
-        //esse_objeto->adicionar_componente<transform_>(transform_());
-        //tf = esse_objeto->pegar_componente<transform_>();
-        //tf->UI = true;
+        // esse_objeto->adicionar_componente<transform_>(transform_());
+        // tf = esse_objeto->pegar_componente<transform_>();
+        // tf->UI = true;
 
         Material mat;
         mat.texturas[0] = ManuseioDados::carregar_Imagem("resources/Textures/null.svg");
-
         mat.shad = "resources/Shaders/ui_componente";
-        mat.cor = vec4(1,0,0,1);
-        border_obj = novo_objeto_jogo();
-        border_obj->adicionar_componente<transform_>(transform_());
-        border_obj->pegar_componente<transform_>()->UI = true;
-        border_obj->adicionar_componente<render_shader>(render_shader());
-        border_obj->pegar_componente<render_shader>()->mat = mat;
-        border_obj->pegar_componente<render_shader>()->camada = render_layer;
-        cena_objetos_selecionados->adicionar_objeto(esse_objeto,border_obj);
-        
-        mat.cor = vec4(0,1,0,1);
+
+        mat.cor = vec4(0, 1, 0, 1);
         background_obj = novo_objeto_jogo();
         background_obj->adicionar_componente<transform_>(transform_());
         background_obj->pegar_componente<transform_>()->UI = true;
         background_obj->adicionar_componente<render_shader>(render_shader());
         background_obj->pegar_componente<render_shader>()->mat = mat;
         background_obj->pegar_componente<render_shader>()->camada = render_layer;
-        cena_objetos_selecionados->adicionar_objeto(esse_objeto,background_obj);
+        cena_objetos_selecionados->adicionar_objeto(esse_objeto, background_obj);
 
-        mat.cor = vec4(0,0,1,1);
+        mat.cor = vec4(1, 0, 0, 1);
+        border_obj = novo_objeto_jogo();
+        border_obj->adicionar_componente<transform_>(transform_());
+        border_obj->pegar_componente<transform_>()->UI = true;
+        border_obj->adicionar_componente<render_shader>(render_shader());
+        border_obj->pegar_componente<render_shader>()->mat = mat;
+        border_obj->pegar_componente<render_shader>()->camada = render_layer;
+        cena_objetos_selecionados->adicionar_objeto(esse_objeto, border_obj);
+
+        mat.cor = vec4(0, 0, 1, 1);
         mat.shad = "resources/Shaders/text";
         text_obj = novo_objeto_jogo();
         text_obj->adicionar_componente<transform_>(transform_());
@@ -91,52 +92,49 @@ public:
         text_obj->pegar_componente<render_texto>()->camada = render_layer;
         text_obj->pegar_componente<render_texto>()->texto = L"ola mundo\nx_X_x\nola mundo";
         text_obj->pegar_componente<render_texto>()->font = ManuseioDados::carregar_fonte("resources/Fonts/Glowworm Regular.json");
-        cena_objetos_selecionados->adicionar_objeto(esse_objeto,text_obj);
-        
-
-        
-
+        cena_objetos_selecionados->adicionar_objeto(esse_objeto, text_obj);
     }
 
-	void atualisar() {
+    void atualisar()
+    {
 
-        if(esse_objeto->pai != NULL && esse_objeto->pai->pegar_componente<ui_componente>() != NULL){
+        /*
+        if (esse_objeto->pai != NULL && esse_objeto->pai->pegar_componente<ui_componente>() != NULL)
+        {
             father = esse_objeto->pai->pegar_componente<ui_componente>();
             global_position += father->global_position + global_position;
-        }else{
+        }
+        else
+        {
             father = NULL;
             global_position = position;
         }
+        */
 
-        //vec3 pos = background_obj->pegar_componente<transform_>()->pos;
-        //print({"background_obj pos",pos.x,pos.y,pos.z});
+        ui_style current_state = normal_style;
 
-        //pos = border_obj->pegar_componente<transform_>()->pos;
-        //print({"border_obj pos",pos.x,pos.y,pos.z});
+        auto mix = [](float x, float y, float t)
+        {
+            return x * (1.0f - t) + y * t;
+        };
 
-        
-        text_obj->pegar_componente<transform_>()->pos = vec3(0,0,0);
-        text_obj->pegar_componente<transform_>()->esca = vec3(0.1,0.1,1);
-        text_obj->pegar_componente<transform_>()->mudar_angulo_graus(vec3(0,0,0));
-        /**/
+        vec2 acurate_pos = vec2(mix(-1,1,global_position.x + position.x),mix(-1,1,global_position.y + position.y));
 
-        
-        background_obj->pegar_componente<transform_>()->pos = vec3(-0.05,0.05,0);
-        background_obj->pegar_componente<transform_>()->esca = vec3(0.1,0.1,1);
-        background_obj->pegar_componente<transform_>()->mudar_angulo_graus(vec3(0,0,0));
-        /**/
+        text_obj->pegar_componente<transform_>()->pos = vec3(acurate_pos.x, acurate_pos.y, 0);
+        text_obj->pegar_componente<transform_>()->esca = vec3(scale.x * 0.1, scale.y * 0.1, 1);
+        text_obj->pegar_componente<transform_>()->mudar_angulo_graus(vec3(0, 0, 0));
 
-        
-        border_obj->pegar_componente<transform_>()->pos = vec3(-0.1,0.1,0);
-        border_obj->pegar_componente<transform_>()->esca = vec3(0.2,0.2,1);
-        border_obj->pegar_componente<transform_>()->mudar_angulo_graus(vec3(0,0,0));
-        /**/
-        
-        
+        background_obj->pegar_componente<transform_>()->pos = vec3(acurate_pos.x - (scale.x + current_state.border_size) / 2, acurate_pos.y + (scale.y + current_state.border_size) / 2, 0);
+        background_obj->pegar_componente<transform_>()->esca = vec3(scale.x + current_state.border_size, scale.y + current_state.border_size, 1);
+        background_obj->pegar_componente<transform_>()->mudar_angulo_graus(vec3(0, 0, 0));
 
+        border_obj->pegar_componente<transform_>()->pos = vec3(acurate_pos.x - scale.x / 2, acurate_pos.y + scale.y / 2, 0);
+        border_obj->pegar_componente<transform_>()->esca = vec3(scale.x, scale.y, 1);
+        border_obj->pegar_componente<transform_>()->mudar_angulo_graus(vec3(0, 0, 0));
     }
 
-	void finalisar() {
+    void finalisar()
+    {
         text_obj->excluir();
         background_obj->excluir();
         border_obj->excluir();
@@ -145,11 +143,11 @@ public:
     ~ui_componente() {}
 };
 
-vec2 ui_componente::cursor_position = vec2(0,0);
+vec2 ui_componente::cursor_position = vec2(0, 0);
 
-
-void test_ui(objeto_jogo* father){
-    shared_ptr<objeto_jogo> test_obj =  novo_objeto_jogo();
+void test_ui(objeto_jogo *father)
+{
+    shared_ptr<objeto_jogo> test_obj = novo_objeto_jogo();
     test_obj->adicionar_componente<ui_componente>(ui_componente());
     shared_ptr<ui_componente> uic = test_obj->pegar_componente<ui_componente>();
     uic->camada = 4;
@@ -159,6 +157,5 @@ void test_ui(objeto_jogo* father){
     uic->normal_style = style;
     uic->hover_style = style;
     uic->click_style = style;
-    
-    cena_objetos_selecionados->adicionar_objeto(father,test_obj);
+    cena_objetos_selecionados->adicionar_objeto(father, test_obj);
 }
