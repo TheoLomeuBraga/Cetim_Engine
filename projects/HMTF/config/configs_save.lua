75e4a5f9e6e02baf63b7d95096c23859fbb0aa0a
TheoLomeuBraga <theolomeu@gmail.com>
2023-12-18 16:29:17 -0300

    a

--- projects/HMTF/config/configs_save.lua
+++ projects/HMTF/config/configs_save.lua
@@ -1,1 +1,1 @@
-do local _={full_screen=false,volume=100,mouse_sensitivity=20.099999999999998};return _;end
+do local _={volume=100,full_screen=false,mouse_sensitivity=20.099999999999998};return _;end
