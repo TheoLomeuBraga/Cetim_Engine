e219b9815fcc7b4f9fbfc26f856c0db236c64a48
TheoLomeuBraga <theolomeu@gmail.com>
2023-12-27 10:17:36 -0300

    a

--- documentation/src/fake_router.js
+++ documentation/src/fake_router.js
@@ -1,12 +1,18 @@
 const main_frame = document.getElementById('main_frame');
 main_frame.src = "./pages/hello.html";
 
-class Button_Linker{
-    constructor(id,page){
-        this.id = id
-        this.page = page
+const meuMap = new Map();
+meuMap.set("hello", "./pages/hello.html");
+meuMap.set("guide_lines", "./pages/guide_lines.html");
+
+meuMap.forEach(function (valor, chave) {
+    const elemento = document.getElementById(chave);
+    if (elemento) {
+        elemento.addEventListener("click", function () {
+            main_frame.src = valor;
+        });
+    } else {
+        console.error("Elemento com ID " + chave + " não encontrado.");
     }
-}
-buttons_linkers = [
-    new Button_Linker("hello","./pages/hello.html"),
-]
+});
+
