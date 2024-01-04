45b659c9c6d0765d7c247cecca8d15dba30b6bb8
TheoLomeuBraga <theolomeu@gmail.com>
2024-01-04 12:35:42 +0000

    a

--- projects/HMTF/resources/Scripts/game_scripts/enimy_master.lua
+++ projects/HMTF/resources/Scripts/game_scripts/enimy_master.lua
@@ -1,3 +1,12 @@
+require("components.extras")
+require("components.component_all")
+require("components.component_index")
+require("objects.global_data")
+require("objects.time")
+require("short_cuts.create_sound")
+require("short_cuts.create_mesh")
+require("short_cuts.create_collision")
+
 
 enemys = {}
 
@@ -5,6 +14,14 @@ function START()
 end
 
 function UPDATE()
+    local update_entity_map = {
+        test_enemy = function (enemy)
+            print("AAAAA")
+        end,
+    }
+    for index, value in ipairs(enemys) do
+        update_entity_map[value.type](value)
+    end
 end
 
 function COLLIDE(collision_info)
@@ -16,6 +33,30 @@ end
 
 
 
-function summon_enemy(pos,rot_y,type)
+function summon_enemy(args)
+    local pos = args.pos
+    local rot_y = args.rot_y
+    local type = args.type
+
+    local obj = game_object(create_object(global_data.layers.main))
+    obj.components.transform.position = deepcopy(pos)
+    obj.components.transform.rotation = {x=0,y=rot_y,z=0}
+    obj.components.transform:set()
+
+    local actions_per_type = {
+        test_enemy = function ()
+            print("AAAAA")
+        end,
+    }
+
+    actions_per_type[type]()
+
+    local enemy = {
+        type = type,
+        obj = obj,
+        animation = "",
+        animation_time = 0,
+    }
+
     return {}
 end
