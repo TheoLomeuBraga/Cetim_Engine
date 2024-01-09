fb5d30fac6bcfec80359d19fe94cb4c87a8b0214
TheoLomeuBraga <theolomeu@gmail.com>
2024-01-08 22:33:19 -0300

    a

--- projects/HMTF/resources/Scripts/game_scripts/enemy_master.lua
+++ projects/HMTF/resources/Scripts/game_scripts/enemy_master.lua
@@ -6,6 +6,8 @@ require("objects.time")
 require("short_cuts.create_sound")
 require("short_cuts.create_mesh")
 require("short_cuts.create_collision")
+require("resources.playable_scene")
+
 
 
 enemys = {}
@@ -41,23 +43,32 @@ function summon_enemy(args)
 
     local obj = game_object(create_object(global_data.layers.main))
     obj.components.transform.position = deepcopy(pos)
+    print("AAAAA",pos.x,pos.y,pos.z)
     obj.components.transform.rotation = {x=0,y=rot_y,z=0}
     obj.components.transform:set()
 
+    local enemy = {
+        type = type,
+        obj = obj,
+        rig_obj = nil,
+        animation = "",
+        animation_time = 0,
+    }
+
     local actions_per_type = {
         test_enemy = function ()
-            local model_path = "resources/3D Models/test_enemy.gltf"
+            local model_path = "resources/3D Models/test_enimy.gltf"
+
+            local enemy_data = get_scene_3D(model_path)
+            local enemy_structures = cenary_builders.entity(obj.object_ptr, 4, enemy_data, "resources/Shaders/mesh", true, false)
+            --enemy.rig_obj = enemy_structures.parts_list
+            
         end,
     }
 
     actions_per_type[type]()
 
-    local enemy = {
-        type = type,
-        obj = obj,
-        animation = "",
-        animation_time = 0,
-    }
+    
 
     table.insert(enemys,enemy)
 
