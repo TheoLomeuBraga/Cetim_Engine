fb7d6c09d2ff867345a11dc3069de0a979f4c0b6
TheoLomeuBraga <theolomeu@gmail.com>
2024-02-23 10:31:59 -0300

    a

--- src/Cetim_Engine_Core/components/navmesh.h
+++ src/Cetim_Engine_Core/components/navmesh.h
@@ -335,9 +335,13 @@ public:
         if (esse_objeto->pegar_componente<transform_>())
         {
 
+            navMesh = new dtNavMesh();
+
             glm::mat4 tfm = esse_objeto->pegar_componente<transform_>()->pegar_matriz();
             internal_path_mesh = apply_transformation_to_mesh(path_mesh, tfm);
-            rcmeshes = navmesh_convertToRcPolyMesh(internal_path_mesh);
+            print("AAAAA",internal_path_mesh.get());
+            rcmeshe = navmesh_convertToRcPolyMesh(internal_path_mesh);
+            print("BBBBB",rcmeshe);
 
             dtNavMeshCreateParams params;
             memset(&params, 0, sizeof(params));
@@ -372,8 +376,6 @@ public:
             params.polyAreas = polyAreas;
             params.polyFlags = polyFlags;
 
-            unsigned char *navData;
-            int navDataSize;
             if (!dtCreateNavMeshData(&params, &navData, &navDataSize))
             {
                 clear();
@@ -381,7 +383,6 @@ public:
             }
 
             // Inicializar dtNavMesh com os dados de navegação
-
             if (dtStatusFailed(navMesh->init(navData, navDataSize, DT_TILE_FREE_DATA)))
             {
                 clear();
