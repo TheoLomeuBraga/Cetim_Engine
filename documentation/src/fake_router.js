7ca0b8d090235b3a393fbbdd22ca8d66307b2830
TheoLomeuBraga <theolomeu@gmail.com>
2024-02-28 14:26:46 -0300

    a

--- documentation/src/fake_router.js
+++ documentation/src/fake_router.js
@@ -5,6 +5,7 @@ const meuMap = new Map();
 meuMap.set("hello", "./pages/hello.html");
 meuMap.set("guide_lines", "./pages/guide_lines.html");
 
+
 meuMap.forEach(function (valor, chave) {
     const elemento = document.getElementById(chave);
     if (elemento) {
