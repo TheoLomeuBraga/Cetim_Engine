e529c90e88306ec7d63e4885dc0ba242b67aa749
TheoLomeuBraga <theolomeu@gmail.com>
2024-03-04 19:03:41 -0300

    a

--- projects/HMTF/resources/Scripts/game_scripts/navegation_test.lua
+++ projects/HMTF/resources/Scripts/game_scripts/navegation_test.lua
@@ -1,6 +1,14 @@
 require("engine_libs.function_sets.navmesh")
+require("engine_libs.short_cuts.create_mesh")
+
+this_object = nil
 
 function START()
+
+    this_object = game_object(this_object_ptr)
+
+    create_mesh(this_object_ptr,false,{x=0,y=0,z=0},{x=0,y=0,z=0},{x=1,y=1,z=1},2,nil,nil)
+    
 end
 
 function UPDATE()
