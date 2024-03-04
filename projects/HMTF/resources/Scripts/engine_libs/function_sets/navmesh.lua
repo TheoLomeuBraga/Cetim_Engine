c30baea9f65064c74c3cf8d1eedb6e6c0643419f
TheoLomeuBraga <theolomeu@gmail.com>
2024-03-04 16:56:31 -0300

    a

--- projects/HMTF/resources/Scripts/engine_libs/function_sets/navmesh.lua
+++ projects/HMTF/resources/Scripts/engine_libs/function_sets/navmesh.lua
@@ -25,21 +25,8 @@ function look_to(start,goal,rot_x,rot_y,rot_z)
     return c_look_to(start,goal,rot_x,rot_y,rot_z)
 end
 
-register_function_set("debug")
-
-walk_data_meta_table = {
-    __call = function(self)
-        return {
-            path_segment = 0,
-            follow_up_progression = 0,
-        }
-    end
-}
-
-walk_data = {}
-
-setmetatable(walk_data,walk_data_meta_table)
-
 function walk_along_the_path(path,progression,speed_or_walk_distance)
     return c_walk_along_the_path(path,progression,speed_or_walk_distance)
 end
+
+
