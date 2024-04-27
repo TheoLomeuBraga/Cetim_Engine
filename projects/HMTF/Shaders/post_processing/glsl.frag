e8135001e77257d2b1545ffd58789dc7f60bebf8
TheoLomeuBraga <theolomeu@gmail.com>
2024-04-27 17:55:51 -0300

    a

--- projects/HMTF/Shaders/post_processing/glsl.frag
+++ projects/HMTF/Shaders/post_processing/glsl.frag
@@ -5,8 +5,6 @@ layout(location = 1) in vec2 uv;
 
 layout(location = 0) out vec4 ret;
 
-
-
 // material
 uniform sampler2D post_procesing_render_input[6];
 uniform sampler2D textures[6];
@@ -18,41 +16,8 @@ vec2 re_pos_uv(vec2 UV, vec4 UV_PosSca) {
     return UV * UV_PosSca.zw + UV_PosSca.xy;
 }
 
-const float dither[16] = float[](
-        1.0 / 17.0,  9.0 / 17.0,  3.0 / 17.0, 11.0 / 17.0,
-        13.0 / 17.0, 5.0 / 17.0,  15.0 / 17.0, 7.0 / 17.0,
-        4.0 / 17.0, 12.0 / 17.0,  2.0 / 17.0, 10.0 / 17.0,
-        16.0 / 17.0, 8.0 / 17.0,  14.0 / 17.0, 6.0 / 17.0
-    );
-
-// Função que aplica o dithering estilo PlayStation 1 a uma textura com ajuste de intensidade
-vec4 applyPSXDithering(sampler2D texture, vec2 uv,float power) {
-    ivec2 textureSize = textureSize(texture,0) * 2;
-    // Padrão de dithering 4x4 (Bayer matrix)
-    
-
-    // Ajusta o padrão de dithering com base na resolução da textura
-    float ditherScale = 4.0; // você pode ajustar este valor para mais ou menos granularidade
-    vec2 scaledUV = uv * vec2(textureSize) / ditherScale;
-
-    // Calcula a posição no padrão de dithering
-    vec2 ditherPos = mod(floor(scaledUV), vec2(4.0, 4.0));
-    int index = int(ditherPos.y * 4.0 + ditherPos.x);
-    
-    // Obtém a cor original da textura
-    vec4 color = texture2D(texture, uv);
-    
-    // Aplica o padrão de dithering
-    float ditherValue = dither[index];
-    float ditherIntensity = (ditherValue - 0.5) * 0.04 * power; // Modula a intensidade do dithering com 'power'
-    vec3 ditheredColor = color.rgb + ditherIntensity;
-
-    return vec4(ditheredColor, color.a);
-}
-
-
 vec4 reduce_color_bits(vec4 color, int bits) {
-    if (bits < 1 || bits > 7) {
+    if(bits < 1 || bits > 7) {
         return color; // Retorna a cor original se bits não está no intervalo [1, 7]
     }
 
@@ -68,11 +33,9 @@ void main() {
     //ret = color * texture(post_procesing_render_input[0], uv);
 
     //adicionar Dithering
-    
 
-    ret = color * reduce_color_bits(applyPSXDithering(post_procesing_render_input[0], uv,1),4);
-    
+    ret = color * reduce_color_bits(texture2D(post_procesing_render_input[0], uv), 4);
+
     //ret = color * reduce_color_bits(texture2D(post_procesing_render_input[0], uv),4);
-    
 
 }
--- projects/HMTF/Shaders/shader_functions.glsl
+++ projects/HMTF/Shaders/shader_functions.glsl
@@ -1,8 +1,8 @@
 // Função que aplica o dithering estilo PlayStation 1 a uma textura com ajuste de intensidade
-vec4 applyPSXDithering(sampler2D texture, vec2 uv,float power) {
-    ivec2 textureSize = textureSize(texture,0) * 2;
+const float dither[16] = float[](1.0 / 17.0, 9.0 / 17.0, 3.0 / 17.0, 11.0 / 17.0, 13.0 / 17.0, 5.0 / 17.0, 15.0 / 17.0, 7.0 / 17.0, 4.0 / 17.0, 12.0 / 17.0, 2.0 / 17.0, 10.0 / 17.0, 16.0 / 17.0, 8.0 / 17.0, 14.0 / 17.0, 6.0 / 17.0);
+vec4 applyPSXDithering(sampler2D texture, vec2 uv, float power) {
+    ivec2 textureSize = textureSize(texture, 0) * 2;
     // Padrão de dithering 4x4 (Bayer matrix)
-    
 
     // Ajusta o padrão de dithering com base na resolução da textura
     float ditherScale = 4.0; // você pode ajustar este valor para mais ou menos granularidade
@@ -11,10 +11,10 @@ vec4 applyPSXDithering(sampler2D texture, vec2 uv,float power) {
     // Calcula a posição no padrão de dithering
     vec2 ditherPos = mod(floor(scaledUV), vec2(4.0, 4.0));
     int index = int(ditherPos.y * 4.0 + ditherPos.x);
-    
+
     // Obtém a cor original da textura
     vec4 color = texture2D(texture, uv);
-    
+
     // Aplica o padrão de dithering
     float ditherValue = dither[index];
     float ditherIntensity = (ditherValue - 0.5) * 0.04 * power; // Modula a intensidade do dithering com 'power'
@@ -23,26 +23,18 @@ vec4 applyPSXDithering(sampler2D texture, vec2 uv,float power) {
     return vec4(ditheredColor, color.a);
 }
 
-vec4 applyXBRFilter(sampler2D tex, vec2 uv)
-{
+vec4 applyXBRFilter(sampler2D tex, vec2 uv) {
     ivec2 texSize = textureSize(tex, 0);
     ivec2 uvInt = ivec2(uv * texSize);
     vec2 texOffset = 1.0 / vec2(texSize);
 
     // Coeficientes para o filtro XBR
-    float coef[12] = float[12](
-        1.0 / 256.0, 2.0 / 256.0, 1.0 / 256.0,
-        2.0 / 256.0, 4.0 / 256.0, 2.0 / 256.0,
-        1.0 / 256.0, 2.0 / 256.0, 1.0 / 256.0,
-        2.0 / 256.0, 4.0 / 256.0, 2.0 / 256.0
-    );
+    float coef[12] = float[12](1.0 / 256.0, 2.0 / 256.0, 1.0 / 256.0, 2.0 / 256.0, 4.0 / 256.0, 2.0 / 256.0, 1.0 / 256.0, 2.0 / 256.0, 1.0 / 256.0, 2.0 / 256.0, 4.0 / 256.0, 2.0 / 256.0);
 
     vec4 result = vec4(0.0);
 
-    for (int i = -1; i <= 1; i++)
-    {
-        for (int j = -1; j <= 1; j++)
-        {
+    for(int i = -1; i <= 1; i++) {
+        for(int j = -1; j <= 1; j++) {
             ivec2 offset = ivec2(i, j);
             vec2 sampleUV = (uvInt + offset) * texOffset;
             vec4 sampleColor = texture(tex, sampleUV);
@@ -57,7 +49,7 @@ vec4 applyXBRFilter(sampler2D tex, vec2 uv)
 }
 
 vec4 reduce_color_bits(vec4 color, int bits) {
-    if (bits < 1 || bits > 7) {
+    if(bits < 1 || bits > 7) {
         return color; // Retorna a cor original se bits não está no intervalo [1, 7]
     }
 
