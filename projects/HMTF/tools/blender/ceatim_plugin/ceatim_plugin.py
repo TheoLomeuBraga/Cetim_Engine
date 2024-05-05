de99915e814a294a978b16f600cdb3fb6f4b7029
TheoLomeuBraga <theolomeu@gmail.com>
2024-05-05 17:07:56 -0300

    a

--- projects/HMTF/tools/blender/ceatim_plugin/ceatim_plugin.py
+++ projects/HMTF/tools/blender/ceatim_plugin/ceatim_plugin.py
@@ -9,7 +9,7 @@ def add_player_start(self, context):
     mesh.from_pydata(verts, edges, faces)
     
     # Cria um objeto com esse mesh
-    obj = bpy.data.objects.new("Player_Start", mesh)
+    obj = bpy.data.objects.new("Player Start", mesh)
     context.scene.collection.objects.link(obj)
     
     # Define a propriedade 'type' no objeto criado
@@ -24,21 +24,61 @@ def add_player_start(self, context):
 class OBJECT_OT_add_player_start(bpy.types.Operator):
     """Adicione um cubo comum"""
     bl_idname = "mesh.add_player_start"
-    bl_label = "Common Cube"
+    bl_label = "Player Start"
     bl_options = {'REGISTER', 'UNDO'}
+
+    text = "Player Start"
+    icon='ARMATURE_DATA'
 
     def execute(self, context):
         add_player_start(self, context)
         return {'FINISHED'}
-#bpy.data.window_managers["WinMan"]. = 'STRING'
-#bpy.data.window_managers["WinMan"]. = "type"
+    
+def add_entity(self, context):
+    # Cria um novo mesh personalizado
+    mesh = bpy.data.meshes.new("PlayerStartMesh")
+    verts = [(-1, -1, 0), (1, -1, 0), (1, 1, 0), (-1, 1, 0)]  # coordenadas dos vértices
+    edges = [(0, 1), (1, 2), (2, 3), (3, 0)]  # arestas conectando os vértices
+    faces = [(0, 1, 2, 3)]  # face formada pelos vértices
+    mesh.from_pydata(verts, edges, faces)
+        
+    # Cria um objeto com esse mesh
+    obj = bpy.data.objects.new("Player Entity", mesh)
+    context.scene.collection.objects.link(obj)
+        
+    # Define a propriedade 'type' no objeto criado
+    obj["type"] = "entity"
+    obj["entity_type"] = "test_entity"
+        
+    # Move o objeto para a posição do cursor
+    obj.location = context.scene.cursor.location
 
+    # Define a exibição do objeto como wireframe
+    obj.display_type = 'WIRE'
+
+class OBJECT_OT_add_entity(bpy.types.Operator):
+    """Adicione um cubo comum"""
+    bl_idname = "mesh.add_entity"
+    bl_label = "Entity"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    text ="Entity"
+    icon='GHOST_ENABLED'
+
+    
+
+    def execute(self, context):
+        add_entity(self, context)
+        return {'FINISHED'}
+
+objects_ot = [OBJECT_OT_add_player_start,OBJECT_OT_add_entity]
 
 
 
 def draw_item(self, context):
     layout = self.layout
-    layout.operator(OBJECT_OT_add_player_start.bl_idname, text="Player Start")
+    for o in objects_ot:
+        layout.operator(o.bl_idname, text=o.text)
 
 def add_menu(self, context):
     self.layout.menu("VIEW3D_MT_ceatim_engine", text="Ceatim Engine",icon="PREFERENCES")
@@ -48,15 +88,19 @@ class VIEW3D_MT_ceatim_engine(bpy.types.Menu):
     bl_idname = "VIEW3D_MT_ceatim_engine"
 
     def draw(self, context):
-        self.layout.operator(OBJECT_OT_add_player_start.bl_idname, text="Player Start",icon='ARMATURE_DATA')
+
+        for o in objects_ot:
+            self.layout.operator(o.bl_idname, text=o.text,icon=o.icon)
 
 def register():
-    bpy.utils.register_class(OBJECT_OT_add_player_start)
+    for o in objects_ot:
+        bpy.utils.register_class(o)
     bpy.utils.register_class(VIEW3D_MT_ceatim_engine)
     bpy.types.VIEW3D_MT_add.append(add_menu)
 
 def unregister():
-    bpy.utils.unregister_class(OBJECT_OT_add_player_start)
+    for o in objects_ot:
+        bpy.utils.unregister_class(o)
     bpy.utils.unregister_class(VIEW3D_MT_ceatim_engine)
     bpy.types.VIEW3D_MT_add.remove(add_menu)
 
