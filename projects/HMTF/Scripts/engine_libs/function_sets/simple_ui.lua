23799258611cd4943429a159da7b0979606e40ea
TheoLomeuBraga <theolomeu@gmail.com>
2024-05-10 23:43:43 -0300

    a

--- projects/HMTF/Scripts/engine_libs/function_sets/simple_ui.lua
+++ projects/HMTF/Scripts/engine_libs/function_sets/simple_ui.lua
@@ -1,20 +1,20 @@
 register_function_set("ui")
 
-function simple_ui_transform(position,scale,is_ui,rotation)
+function simple_ui_transform(position, scale, is_ui, rotation)
     if position == nil then
-        position = {x=0,y=0,z=0}
+        position = { x = 0, y = 0, z = 0 }
     end
     if scale == nil then
-        scale = {x=1,y=1,z=1}
+        scale = { x = 1, y = 1, z = 1 }
     end
     if is_ui == nil then
         is_ui = true
     end
     if rotation == nil then
-        rotation = {x=0,y=0,z=0}
+        rotation = { x = 0, y = 0, z = 0 }
     end
 
-    local oposit_bool = function (b)
+    local oposit_bool = function(b)
         if b then
             return false
         end
@@ -29,18 +29,18 @@ function simple_ui_transform(position,scale,is_ui,rotation)
     }
 end
 
-function simple_ui_style(color,border_size,border_color,text_color)
+function simple_ui_style(color, border_size, border_color, text_color)
     if color == nil then
-        color = {r=0.5,g=0.5,b=0.5,a=1}
+        color = { r = 0.5, g = 0.5, b = 0.5, a = 1 }
     end
     if border_size == nil then
         border_size = 0
     end
     if border_color == nil then
-        border_color = {r=0,g=0,b=0,a=1}
+        border_color = { r = 0, g = 0, b = 0, a = 1 }
     end
     if text_color == nil then
-        text_color = {r=0,g=0,b=0,a=1}
+        text_color = { r = 0, g = 0, b = 0, a = 1 }
     end
     return {
         color = color,
@@ -63,8 +63,8 @@ local base_styles = {
 function set_simple_ui_display(style)
     base_styles["simple_ui_display"] = style
 end
-function simple_ui_display(ui_transform,text,ui_style)
 
+function simple_ui_display(ui_transform, text, ui_style)
     local ui_info = {}
     for key, value in pairs(ui_transform) do
         ui_info[key] = ui_transform[key]
@@ -87,44 +87,68 @@ function simple_ui_display(ui_transform,text,ui_style)
     c_simple_ui_display(ui_info)
 end
 
-function simple_ui_lable(ui_transform,ui_style)
-    simple_ui_display(ui_transform,"",ui_style)
+function simple_ui_lable(ui_transform, ui_style)
+    simple_ui_display(ui_transform, "", ui_style)
 end
 
-function simple_ui_text(ui_transform,text,ui_style)
+function simple_ui_text(ui_transform, text, ui_style)
     if ui_style == nil then
         ui_style = simple_ui_style()
     end
     ui_style.color.a = 0
     ui_style.border_color.a = 0
-    simple_ui_display(ui_transform,text,ui_style)
+    simple_ui_display(ui_transform, text, ui_style)
 end
 
-local simple_ui_cursor_position = {x=0,y=0}
-local simple_ui_cursor_movement = {x=0,y=0}
+local simple_ui_cursor_position = { x = 0, y = 0 }
+local simple_ui_cursor_movement = { x = 0, y = 0 }
 local simple_ui_joystick_selection = -1
+local simple_ui_confirm_last_frame = 0
 local simple_ui_confirm = 0
-function set_selection_state(cursor_position,ursor_movement,ui_joystick_id,confirm)
+function set_selection_state(cursor_position, ursor_movement, ui_joystick_id, confirm)
     simple_ui_cursor_position = cursor_position
     simple_ui_cursor_movement = ursor_movement
     simple_ui_joystick_selection = ui_joystick_id
+    simple_ui_confirm_last_frame = simple_ui_confirm
     simple_ui_confirm = confirm
 end
-function is_selected(ui_transform,ui_joystick_id)
 
+function is_cliked(ui_transform, ui_joystick_id)
+    if simple_ui_confirm_last_frame == 0 then
+        if ui_joystick_id == simple_ui_joystick_selection then
+            if simple_ui_confirm == 1 then
+                return 1
+            else
+                return 0.5
+            end
+        end
+
+        local cpt = { x = (simple_ui_cursor_position.x * 2) - 1, y = (((1 - simple_ui_cursor_position.y) * 2) - 1) }
+        if ui_transform.position.x - ui_transform.scale.x < cpt.x and ui_transform.position.x + ui_transform.scale.x > cpt.x and ui_transform.position.y - ui_transform.scale.y < cpt.y and ui_transform.position.y + ui_transform.scale.y > cpt.y then
+            if simple_ui_confirm == 1 then
+                return 1
+            else
+                return 0.5
+            end
+        end
+    end
+    return 0
+end
+
+function is_holded(ui_transform, ui_joystick_id)
     if ui_joystick_id == simple_ui_joystick_selection then
         if simple_ui_confirm == 1 then
             return 1
-        else 
+        else
             return 0.5
         end
     end
 
-    local cpt = {x=(simple_ui_cursor_position.x * 2) - 1,y=(((1 - simple_ui_cursor_position.y) * 2) - 1)}
+    local cpt = { x = (simple_ui_cursor_position.x * 2) - 1, y = (((1 - simple_ui_cursor_position.y) * 2) - 1) }
     if ui_transform.position.x - ui_transform.scale.x < cpt.x and ui_transform.position.x + ui_transform.scale.x > cpt.x and ui_transform.position.y - ui_transform.scale.y < cpt.y and ui_transform.position.y + ui_transform.scale.y > cpt.y then
         if simple_ui_confirm == 1 then
             return 1
-        else 
+        else
             return 0.5
         end
     end
@@ -132,16 +156,15 @@ function is_selected(ui_transform,ui_joystick_id)
     return 0
 end
 
-function simple_ui_button(ui_transform,text,ui_style,ui_joystick_id)
-    simple_ui_display(ui_transform,text,ui_style)
-    return is_selected(ui_transform,ui_joystick_id)
+function simple_ui_button(ui_transform, text, ui_style, ui_joystick_id)
+    simple_ui_display(ui_transform, text, ui_style)
+    return is_cliked(ui_transform, ui_joystick_id)
 end
 
-function simple_ui_slider(ui_transform,text,ui_style,ui_joystick_id)
-    simple_ui_display(ui_transform,text,ui_style)
-    if is_selected(ui_transform,ui_joystick_id) == 1 then
-        return {x=simple_ui_cursor_movement.x,y=simple_ui_cursor_movement.y,z=0}
+function simple_ui_slider(ui_transform, text, ui_style, ui_joystick_id)
+    simple_ui_display(ui_transform, text, ui_style)
+    if is_holded(ui_transform, ui_joystick_id) == 1 then
+        return { x = simple_ui_cursor_movement.x, y = simple_ui_cursor_movement.y, z = 0 }
     end
-    return {x=0,y=0,z=0}
+    return { x = 0, y = 0, z = 0 }
 end
-
