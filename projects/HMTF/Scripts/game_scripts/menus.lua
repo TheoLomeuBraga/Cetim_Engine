dcfd354893d4765421bab7a873694214c40b89dc
TheoLomeuBraga <theolomeu@gmail.com>
2024-05-14 13:51:36 -0300

    a

--- projects/HMTF/Scripts/game_scripts/menus.lua
+++ projects/HMTF/Scripts/game_scripts/menus.lua
@@ -124,6 +124,7 @@ function new_game(state, id)
         print("new_game")
         core_obj = game_object(global_data.core_object_ptr)
         core_obj.components.lua_scripts.scripts["core"].functions.load_sceane_request({ "floating_island_hub" })
+        --core_obj.components.lua_scripts.scripts["core"].functions.load_sceane_request({ "hub" })
     end
 end
 
