50946448379f87c87280b6b0977bb5aa5346f2fb
TheoLomeuBraga <theolomeu@gmail.com>
2024-06-21 23:50:44 -0300

    a

--- src/Cetim_Engine_Core/implementacao_glfw.h
+++ src/Cetim_Engine_Core/implementacao_glfw.h
@@ -128,6 +128,8 @@ public:
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
 		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 1);
+
 
 		window = SDL_CreateWindow(pegar_nome_arquivo(argumentos[1]).c_str(), configuracoes::janelaConfig.X, configuracoes::janelaConfig.Y, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 		if (window == nullptr)
